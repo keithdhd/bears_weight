527e4e9de25e8a20bc95cc0ed4cdf6f0e5bd077b
Keith Douglas <keith.douglas@codeclan.com>
2020-10-13 09:34:51 +0100

    adds other options for print message

--- guess_the_weight.py
+++ guess_the_weight.py
@@ -4,3 +4,7 @@ guess = input("Guess the bear's weight: ")
 
 if int(guess) == correct_weight:
   print("Well done! Correct")
+elif int(guess) < correct_weight:
+  print("I'm heavier than that")
+else:
+  print("I'm lighter than that")
