f4efdf0cd37d7cbc1e9d3cf71d9292fc3c33ea91
Keith Douglas <keith.douglas@codeclan.com>
2020-10-13 09:41:42 +0100

    adds some more conditions

--- guess_the_weight.py
+++ guess_the_weight.py
@@ -4,7 +4,11 @@ guess = input("Guess the bear's weight: ")
 
 if int(guess) == correct_weight:
   print("Well done! Correct")
-elif int(guess) < correct_weight:
-  print("I'm heavier than that")
-else:
+elif int(guess) > 50:
   print("I'm lighter than that")
+elif int(guess) < correct_weight and int(guess) > 40:
+  print("I'm heavier than 40 but less than 51")
+elif int(guess) < 40 and int(guess) > 30:
+  print("Close! Try again")
+else:
+  print("I'm heavier than that")
